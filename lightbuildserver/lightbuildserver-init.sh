3c6cd42cb763664a22055ad3d0d02ebbde3dd6b9
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-06-18 22:05:19 +0200

    some changes for default demo configuration, lightbuildserver running container on localhost

--- lightbuildserver/lightbuildserver-init.sh
+++ lightbuildserver/lightbuildserver-init.sh
@@ -6,3 +6,16 @@ chown uwsgi:uwsgi /var/log/uwsgi.log
 usermod -a nginx -G uwsgi
 touch /var/log/uwsgi.log
 chown uwsgi:uwsgi /var/log/uwsgi.log
+
+mkdir -p /var/lib/lbs/ssh
+echo "this key will be used to login to the host for the containers."
+echo "leave the passwort empty!"
+ssh-keygen -t rsa -f /var/lib/lbs/ssh/container_rsa
+
+# for using the local machine as a host for the containers
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo >> ~/.ssh/authorized_keys
+chmod 700 ~/.ssh/authorized_keys
+cat /var/lib/lbs/ssh/container_rsa.pub >> ~/.ssh/authorized_keys
+echo "127.0.0.1   build01.localhost" >> /etc/hosts
