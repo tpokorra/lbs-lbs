3580213f67f46c659ec5f967fa7d72b1328b8ccc
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-07-08 13:30:21 +0000

    small fix for init script. uwsgi log file does not exist yet

--- lightbuildserver/lightbuildserver-init.sh
+++ lightbuildserver/lightbuildserver-init.sh
@@ -2,7 +2,6 @@
 
 chown -R uwsgi:uwsgi /usr/share/lightbuildserver/
 chown uwsgi:uwsgi /etc/uwsgi.d/lightbuildserver.ini
-chown uwsgi:uwsgi /var/log/uwsgi.log
 usermod -a nginx -G uwsgi
 touch /var/log/uwsgi.log
 chown uwsgi:uwsgi /var/log/uwsgi.log
