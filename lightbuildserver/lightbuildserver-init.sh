22bedc42bd20948546efe6590987f5b8b9ccf53a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-08-17 09:32:51 +0000

    lightbuildserver: more fixes in the init script

--- lightbuildserver/lightbuildserver-init.sh
+++ lightbuildserver/lightbuildserver-init.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 chown -R uwsgi:uwsgi /usr/share/lightbuildserver/
+chown -R uwsgi:uwsgi /var/lib/lightbuildserver/
 chown uwsgi:uwsgi /etc/uwsgi.d/lightbuildserver.ini
 usermod -a nginx -G uwsgi
 touch /var/log/uwsgi.log
 chown uwsgi:uwsgi /var/log/uwsgi.log
+
+# disable the default page by moving it to port 81
+sed -i "s/80 default_server/81 default_server/g" /etc/nginx/nginx.conf
 
 etccontainerpath=/etc/lightbuildserver/container
 echo "this key will be used to login to the host for the containers."
