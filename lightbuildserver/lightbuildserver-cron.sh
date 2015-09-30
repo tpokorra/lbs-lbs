74257d7e4cd96d27eccfdf8eb90f17f617d40ca1
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-09-30 14:48:22 +0200

    lbs cron job not against localhost, but the actual IPv4 address

--- lightbuildserver/lightbuildserver-cron.sh
+++ lightbuildserver/lightbuildserver-cron.sh
@@ -3,9 +3,10 @@
 jobruns=`ps xaf | grep -v grep | grep processbuildqueue`
 if [ -z "$jobruns" ]
 then
+  ip4=`ip addr list eth0 | grep "inet "|cut -d' ' -f6|cut -d/ -f1`
   for i in `seq 1 6`
-  do 
-    wget --tries=1 --timeout=3 -q http://localhost/processbuildqueue -O /dev/null > /dev/null
+  do
+    wget --tries=1 --timeout=3 -q http://${ip4}/processbuildqueue -O /dev/null > /dev/null
     sleep 5
   done
 fi
