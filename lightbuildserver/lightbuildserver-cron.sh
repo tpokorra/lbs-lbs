8202d93bdcd02a66e38d2674f955ec09f6d8dbd6
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-10-01 08:47:15 +0200

    lightbuildserver: processbuildqueue: needs to trigger on localhost and on the eth0 IP address uwsgi listens on localhost, running it directly listens on the external IP address

--- lightbuildserver/lightbuildserver-cron.sh
+++ lightbuildserver/lightbuildserver-cron.sh
@@ -6,7 +6,13 @@ then
   ip4=`ip addr list eth0 | grep "inet "|cut -d' ' -f6|cut -d/ -f1`
   for i in `seq 1 6`
   do
-    wget --tries=1 --timeout=3 -q http://${ip4}/processbuildqueue -O /dev/null > /dev/null
+    # need to check both addresses.
+    # when starting the server locally, it listens on the external IP address.
+    # uwsgi listens on localhost
+    for ip in $ip4 localhost
+    do
+      wget --tries=1 --timeout=3 -q http://${ip}/processbuildqueue -O /dev/null > /dev/null
+    done
     sleep 5
   done
 fi
